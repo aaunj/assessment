6c6af9ada1e853cb06a9306719625e390a19e238
aunzyme <aunpeac3@gmail.com>
2023-01-05 11:44:48 +0700

    integ:CreateHandler PASS

--- expense/expense_integration_test.go
+++ expense/expense_integration_test.go
@@ -4,10 +4,12 @@
 package expense
 
 import (
-	"database/sql"
+	"context"
+	_ "database/sql"
 	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
@@ -16,6 +18,7 @@ import (
 	"time"
 
 	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
 )
 
 const serverPort = 2565
@@ -25,14 +28,14 @@ func TestCreateHandler(t *testing.T) {
 	// Setup server
 	eh := echo.New()
 	go func(e *echo.Echo) {
-		db, err := sql.Open("postgres", "postgresql://root:root@db/go-example-db?sslmode=disable")
-		if err != nil {
-			log.Fatal(err)
-		}
+		// conn, err := sql.Open("postgres", "postgresql://root:root@db/assessment?sslmode=disable")
+		// if err != nil {
+		// 	log.Fatal("can't connect to database", err)
+		// }
+		// db = conn
+		InitDB()
 
-		// h := NewApplication(db)
-
-		e.POST("/expenses", expense.CreateHandler)
+		e.POST("/expenses", CreateHandler)
 		e.Start(fmt.Sprintf(":%d", serverPort))
 	}(eh)
 	for {
@@ -44,34 +47,40 @@ func TestCreateHandler(t *testing.T) {
 			conn.Close()
 			break
 		}
+
 	}
-	// // Arrange
-	// reqBody := ``
-	// req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("http://localhost:%d/news", serverPort), strings.NewReader(reqBody))
-	// assert.NoError(t, err)
-	// req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-	// client := http.Client{}
+	// Arrange
+	reqBody := `{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath",
+		"tags": ["food","beverage"]
+	}`
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("http://localhost:%d/expenses", serverPort), strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
 
 	// // Act
-	// resp, err := client.Do(req)
-	// assert.NoError(t, err)
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
 
-	// byteBody, err := ioutil.ReadAll(resp.Body)
-	// assert.NoError(t, err)
-	// resp.Body.Close()
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
 
 	// // Assertions
-	// expected := "[{\"ID\":1,\"Title\":\"test-title\",\"Content\":\"test-content\",\"Author\":\"test-author\"}]"
+	expected := "{\"id\":1,\"title\":\"strawberry smoothie\",\"amount\":79,\"note\":\"night market promotion discount 10 bath\",\"tags\":[\"food\",\"beverage\"]}"
 
-	// if assert.NoError(t, err) {
-	// 	assert.Equal(t, http.StatusOK, resp.StatusCode)
-	// 	assert.Equal(t, expected, strings.TrimSpace(string(byteBody)))
-	// }
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusCreated, resp.StatusCode)
+		assert.Equal(t, expected, strings.TrimSpace(string(byteBody)))
+	}
 
-	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	// defer cancel()
-	// err = eh.Shutdown(ctx)
-	// assert.NoError(t, err)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = eh.Shutdown(ctx)
+	assert.NoError(t, err)
 
 	// eh := setupServer()
 
